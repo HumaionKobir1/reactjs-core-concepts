89eaa749c0f37054f6c804ac8e4ae6eb5ff0bf2f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-23 10:50:57 +0600

    Added => Pass dynamic data to components, props in react

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-const number = 5555;
-const singer  = {name: 'Humaion kobir', job: 'singer'};
-const singer2  = {name: 'touhid', job: 'singer'};
+// const number = 5555;
+// const singer  = {name: 'Humaion kobir', job: 'singer'};
+const singer2  = {name: 'Prince mahmud', job: 'singer'};
 
-const singerStyle = {
-  color: 'purple',
-  backgroundColor: 'white',
-  padding: '4px',
-  borderRadius: '10px'
-}
+// const singerStyle = {
+//   color: 'purple',
+//   backgroundColor: 'white',
+//   padding: '4px',
+//   borderRadius: '10px'
+// }
 
 function App() {
   return (
     <div className="App">
-      <h1>Humaion Kobir</h1>
-      <Person></Person>
-      <Person></Person>
+      <h1>React</h1>
+      <Person name ="Humaion Kobir" nayika = "Beuty akter bithi"></Person>
+      <Person name = "Tanvir rahman" nayika = "Sharmin akter"></Person>
       <Person></Person>
       <h5>New component YEYH!</h5>
-      <Friend></Friend>
+      <Friend phone = "017777"></Friend>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
+  console.log(props);
   return (
     <div className='person'>
-      <h3>REACT</h3>
-      <p>Hey react how are you</p>
+      <h3>{props.name}</h3>
+      <p>{props.nayika}</p>
     </div>
   )
 }
 
-function Friend(){
+function Friend(props){
+  console.log(props);
   return(
     <div className='container'>
-      <h3>Name: Prince mahmud</h3>
-      <p>Job: Polli staf</p>
+      <h3>Name: {singer2.name}</h3>
+      <p>{props.phone}</p>
     </div>
   )
 }
