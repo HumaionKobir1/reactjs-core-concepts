9ddf8f00075b6e34f9a42ca9cc6d7e09e0a9e596
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-23 11:52:07 +0600

    Create multiple components from an array f objects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 // const number = 5555;
-// const singer  = {name: 'Humaion kobir', job: 'singer'};
+const singers  = [
+  {name: 'Humaion kobir', job: 'singer'},
+  {name: 'suvo khan', job: 'banker'},
+  {name: 'agun', job: 'fagun'},
+  {name: 'taskin', job: 'Cricket'}
+];
 const singer2  = {name: 'Prince mahmud', job: 'singer'};
 
 // const singerStyle = {
@@ -13,12 +18,27 @@ const singer2  = {name: 'Prince mahmud', job: 'singer'};
 // }
 
 function App() {
+  const nayoks = ['salman sah','Riaz', 'bapparaz', 'sakil khan', 'ferdows', 'omor sani', 'razzak'];
+  const naykas = ['Mousumi', 'sabnur', 'purnima'];
   return (
     <div className="App">
       <h1>React</h1>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name = {nayok}></Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name} nayika={singer.job}></Person>)
+      }
+
+
+      {/*
+      <Person name = "Tanvir rahman" nayika = "Sharmin akter"></Person> */}
+      <Person name = {nayoks[0]} nayika = {naykas[1]}></Person>
       <Person name ="Humaion Kobir" nayika = "Beuty akter bithi"></Person>
-      <Person name = "Tanvir rahman" nayika = "Sharmin akter"></Person>
-      <Person></Person>
       <h5>New component YEYH!</h5>
       <Friend phone = "017777"></Friend>
     </div>
